8093ae9378e3a17ec645cf793ceda68e7d7e1550
nevicoder <nevi@Nevi-T450s.localdomain>
2022-07-07 15:13:28 +0700

    change avatar link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -13,10 +13,7 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.wrapper}>
         <div className={styles.avatar}>
-          <img
-            src="https://scontent.fvca1-2.fna.fbcdn.net/v/t39.30808-6/271546716_2257043321109788_7855038822841370437_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=s1xnCAFwkXQAX8jM1bd&_nc_ht=scontent.fvca1-2.fna&oh=00_AT_IPz0blv1rLMX1L5iBeL_vhVI7CurSi4G_19Aaq81JpA&oe=62C51A27"
-            alt="avatar"
-          />
+          <img src="src/assets/avatar.jpg" />
         </div>
         <div className={classnames(styles.title, styles.nonSelect)}>
           <h1>Hi, welcome to my portfolio</h1>
